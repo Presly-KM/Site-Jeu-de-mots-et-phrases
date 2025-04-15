df0dc316537b3bc4d2c2d6d12b606bf7acc5c3da
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-04-15 21:50:15 +0200

    scripts push

---
+++ scripts/config.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+
+// Comme ces deux variables ne sont pas dans un bloc de code elles auront une portée globale (et seront parfaitement utilisable) non seulemnt pour config.js mais ausi pour "l'onglet" script.js
